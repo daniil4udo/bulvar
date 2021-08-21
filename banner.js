361d5628bf14d1a7b2b3bef63d8b60830bb3b940
Daniil <daniil.chumachenko@democrance.com>
2021-08-21 10:22:05 +0400

    change banner

--- banner.js
+++ banner.js
@@ -1,7 +1,13 @@
 'use strict'
 const pack = require('./package.json')
+const year = new Date().getFullYear()
+const homepage = `(${pack.homepage})` || ''
 
-const bannerTxt = `/*! bulma-buefy-css-variables v${pack.version} | MIT License | github.com/dino4udo/bulma-buefy-css-variables */\n`
+const bannerTxt = `/*!
+  * bulma-buefy-css-variables v${pack.version} ${homepage}
+  * Copyright ${year} ${pack.author.name} ${pack.author.email}
+  * Licensed under MIT (https://github.com/dino4udo/bulma-buefy-css-variables/blob/main/LICENSE)
+  */\n`
 
 process.stdout.write(bannerTxt)
 process.stdin.pipe(process.stdout)
