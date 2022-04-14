56318d62eaff0ad61d84d3e9e04d6c0c556f25d2
daniil4udo <daniil4udo@gmail.com>
2022-04-14 11:14:17 +0400

    feature: ✨  postcss plugin to prefix user-select

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,39 @@
-module.exports = ctx => {
-  return {
-    map: {
-        inline: false,
-        annotation: true,
-        sourcesContent: true
-      },
-    plugins: {
-      autoprefixer: {
-        cascade: false
-      },
-      // TODO: need some testing
-      // rtlcss: ctx.env === 'RTL' ? {} : false
-    }
-  }
-}
+const map = {
+	none: 'none',
+	auto: 'default',
+	text: 'default',
+	contain: 'default',
+	all: 'default',
+	inherit: 'inherit',
+	initial: 'initial',
+	unset: 'unset',
+};
+
+const postcssTouchCallout = (root) =>
+	root.walkDecls('user-select', (declaration) => {
+		if (declaration.value in map) {
+			declaration.parent.prepend({
+				prop: '-webkit-touch-callout',
+				value: map[declaration.value],
+			});
+
+
+		}
+	});
+
+module.exports = (ctx) => {
+	return {
+		map: {
+			inline: false,
+			annotation: true,
+			sourcesContent: true,
+		},
+		plugins: [
+			require('autoprefixer')({
+				cascade: false,
+			}),
+			postcssTouchCallout,
+			ctx.env === 'RTL' ? require('rtlcss')({}) : () => {},
+		],
+	};
+};
