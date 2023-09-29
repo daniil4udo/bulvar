e6ceda2145b651a28ed4e889ee4c81400dcaed70
daniil4udo <daniil4udo@gmail.com>
2023-09-29 13:31:23 +0400

    dep-up(lint): 📦  configs and packages

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+	$schema: 'http://json.schemastore.org/prettierrc',
+	semi: true,
+	arrowParens: 'always',
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	insertPragma: false,
+	tabWidth: 4,
+	useTabs: false,
+	vueIndentScriptAndStyle: true,
+	endOfLine: 'lf',
+};
